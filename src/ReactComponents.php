c57bded463d33718ef86b88b348d954dba658f6c
Isaac Abensur <isaacabensur@gmail.com>
2019-03-13 17:09:08 -0500

    Create ReactComponents.php

---
+++ src/ReactComponents.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jvasc
+ * Date: 1/9/2017
+ * Time: 12:57
+ */
+
+namespace BluefireDev\autoform;
+
+// JVA - 01-09-2017
+// Clase de constantes para la invocación de componentes de React en los blades
+class ReactComponents
+{
+    const LARAVEL_CRUD_COMPONENT = "laravel-crud-component";
+}
